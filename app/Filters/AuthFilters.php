1671c1845994427937015163808843fe68f84fb7
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2025-06-01 19:34:47 +0900

    filter 5

--- app/Filters/AuthFilters.php
+++ app/Filters/AuthFilters.php
@@ -5,7 +5,7 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
 
-class Auth implements FilterInterface
+class AuthFilters implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
